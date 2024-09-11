0a6bcedb829d7c86d56b1893514102211c55573a
HeitorMorais <heitormorais2003@gmail.com>
2024-09-11 11:04:32 -0300

    aula 05: agenda contato usando api do OKHTTP e GSON, funcoes de gravar e pesquisar funcionando

---
+++ mobile-I/aula-05/agenda/app/src/main/java/com/example/agendacontato/Contato.kt
@@ -0,0 +1,13 @@
+package com.example.agendacontato
+
+data class Contato (
+  var nome : String = "", var email: String = "", var telefone: String = ""
+) {
+}
+
+//fun main() {
+//    val c1 = Contato (
+//        "Joao", "joao@teste.com", "(11) 91119-1199"
+//    )
+//    println(c1)
+//}
